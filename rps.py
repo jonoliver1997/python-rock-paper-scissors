0fa7b76432c7d249f90597259ffc6b7e97fdc996
Jonathan Oliver <jonoliver1997@gmail.com>
2024-03-01 20:20:50 -0800

    initial repo

---
+++ rps.py
@@ -0,0 +1,37 @@
+import sys
+import random
+from enum import Enum
+
+class RPS(Enum):
+  ROCK = 1
+  PAPER = 2
+  SCISSORS = 3
+
+
+
+print('')
+playerchoice = input("Enter...\n1 for Rock\n2 for Paper\n3 for Scissors:\n\n")
+
+player = int(playerchoice)
+
+if player < 1 or player > 3:
+  sys.exit("You must enter 1, 2, or 3")
+
+computerchoice = random.choice("123")
+computer = int(computerchoice)
+
+print('')
+print("You chose", str(RPS(player)).replace("RPS.", "").lower())
+print("Python chose", str(RPS(computer)).replace("RPS.", "").lower())
+print('')
+
+if player == computer:
+  print("😱 It's a tie!")
+elif player == 1 and computer == 3:
+  print("🎉 You win! Rock beats Scissors")
+elif player == 2 and computer == 1:
+  print("🎉 You win! Paper beats Rock")
+elif player == 3 and computer == 2:
+  print("🎉 You win! Scissors beats Paper")
+else:
+  print("🐍 Python Wins!")
